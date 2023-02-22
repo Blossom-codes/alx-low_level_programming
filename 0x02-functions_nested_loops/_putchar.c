6d4c1a2730cc0d8f66bc7fef86ccd435da2fbc36
Blossom-codes <blozzyblack@gmail.com>
2023-02-21 20:11:55 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h";
+#include <unstd.h>;
+/**
+ * main - prints _putchar, followed by a new line.
+ * @c The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and error is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
